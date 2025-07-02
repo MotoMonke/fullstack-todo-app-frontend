f4940fc320d7bd1d3b58008c5db1b84869e980d5
Daniil <developmentweb15@gmail.com>
2025-07-02 12:20:41 +0300

    Finished getData page functionality

--- src/pages/GetData.tsx
+++ src/pages/GetData.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import type { TodoList } from "../types";
 function GetData(){
@@ -26,22 +25,75 @@ function GetData(){
             list
         ]);
     }
+    function unstage(list:TodoList){
+        setDataToSave(dataToSave.filter(li=>li._id!==list._id));
+        setFetchedData([
+            ...fetchedData,
+            list
+        ]);
+    }
+    function saveData(){
+        const localStorageData = localStorage.getItem('DATA'); 
+        if(localStorageData){
+            let parsed:TodoList[] = JSON.parse(localStorageData);
+            dataToSave.forEach(list=>{
+                //checking if todo list with that _id already exist localy
+                const found = parsed.find((element)=>element._id===list._id);
+                if(found){
+                    //overwriting local todo list by downloaded list if _id mathces
+                    const updated = parsed.map(element=>{
+                        if(element._id===found._id){
+                            return found;
+                        } else {
+                            return element;
+                        }
+                    });
+                    parsed = updated;
+                } else {
+                    parsed.push(list);
+                }
+            });
+            localStorage.setItem('DATA',JSON.stringify(parsed));
+            setDataToSave([]);
+        } else {
+            localStorage.setItem('DATA',JSON.stringify(dataToSave));
+            setDataToSave([]);
+        }
+    }
+    async function deleteData(_id:String){
+        try {
+            const res = await axios.delete("http://localhost:5001/api/todo/delete",{ 
+                data:{list_id:_id},
+                withCredentials: true});
+            if(res.status===200){
+                setFetchedData(fetchedData.filter(list => list._id !== _id));
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
         <>
             <div className="get-data">
-                <button onClick={getData}>Get data</button>
+                <button disabled={fetchedData.length>0||dataToSave.length>0} onClick={getData}>Get data</button>
                 <div className="fetched-data-container">
                     {fetchedData.map(list=>(
-                        <div key={list._id} onClick={()=>selectToSave(list)}>{list.name}</div>
+                        <div key={list._id} className="fetched-todo-list">
+                            <div onClick={()=>selectToSave(list)}>{list.name}</div>
+                            <button onClick={()=>deleteData(list._id)}>Delete from cloud</button>
+                        </div>
                     ))}
                 </div>
             </div>
             <div className="save-data">
                 <h1>Data to save</h1>
                 {dataToSave.map(list=>(
-                    <div key={list._id}>{list.name}</div>
+                    <div key={list._id} onClick={()=>unstage(list)}>{list.name}</div>
                 ))}
+                <button disabled={dataToSave.length===0} onClick={saveData}>Save data locally</button>
             </div>
+            <a href="/">Go to main page</a>
         </>
     );
 }
